a0b0b5106256347ac6ef0cfc3b32f871d36ab96e
Ivoneideduarte <contato.ivoneideduarte@gmail.com>
2025-06-18 23:48:55 -0300

    Inserindo texto no search

---
+++ receita/App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Routes } from './src/routes'
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+
+  );
+}
+
