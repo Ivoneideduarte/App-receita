1df6b3445a4b12f9a05b03b253e38473867537c5
Ivoneideduarte <contato.ivoneideduarte@gmail.com>
2025-06-21 20:00:30 -0300

    Animações

--- receita/App.js
+++ receita/App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler'
 import { StyleSheet, Text, View } from 'react-native';
 import { Routes } from './src/routes'
 import { NavigationContainer } from '@react-navigation/native';
--- receita/src/pages/home/index.js
+++ receita/src/pages/home/index.js
@@ -14,6 +14,7 @@ import api from '../../services/api'
 import { FoodList } from '../../components/foodlist'
 
 import { useNavigation } from '@react-navigation/native';
+import { Text as MotiText } from 'moti';
 
 export function Home() {
 
@@ -48,8 +49,38 @@ export function Home() {
         <SafeAreaView style={styles.container}>
             <Logo />
 
-            <Text style={styles.title}>Encontre a receita</Text>
-            <Text style={styles.title}>que combina com você</Text>
+            <MotiText 
+                style={styles.title}
+                from={{
+                    opacity: 0,
+                    translateY: 15
+                }}    
+                animate={{
+                    opacity: 1,
+                    translateY: 0
+                }}
+                transition={{
+                    delay: 100,
+                    type: 'timing',
+                    duration: 650
+                }}
+            >Encontre a receita</MotiText>
+            <MotiText 
+                style={styles.title}
+                from={{
+                    opacity: 0,
+                    translateY: 18
+                }}    
+                animate={{
+                    opacity: 1,
+                    translateY: 0
+                }}
+                transition={{
+                    delay: 200,
+                    type: 'timing',
+                    duration: 850
+                }}
+            >que combina com você</MotiText>
 
             <View style={styles.form}>
 
